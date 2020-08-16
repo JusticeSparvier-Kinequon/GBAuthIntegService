856e45e087f42fc33b9f0ce5f0d8ddf6f5f48108
Justice Sparvier-Kinequon <47434969+JusticeSparvier-Kinequon@users.noreply.github.com>
2020-08-15 22:00:41 -0700

    Added boilerplate for GBAuthInteg

---
+++ src/main/java/com/ganbru/authinteg/AuthIntegApplication.java
@@ -0,0 +1,13 @@
+package com.ganbru.authinteg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthIntegApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthIntegApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ganbru/authinteg/AuthIntegApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ganbru.authinteg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthIntegApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
